d0ddbfa7388711a82007c9f561c71c39cce6867e
schmelz-ctr <81192860+schmelz-ctr@users.noreply.github.com>
2021-06-01 09:26:29 -0400

    Updated constructors with history

--- src/pe_reports/__init__.py
+++ src/pe_reports/__init__.py
@@ -1,13 +1,8 @@
-"""The example library."""
+"""The pe_reports library."""
 # We disable a Flake8 check for "Module imported but unused (F401)" here because
 # although this import is not directly used, it populates the value
 # package_name.__version__, which is used to get version information about this
 # Python package.
-from ._version import __version__  # noqa: F401
-<<<<<<< HEAD
-from .example import example_div
-
-__all__ = ["example_div"]
-=======
-from .report_generator import main
->>>>>>> 941169fb38926b928e08597e53869cc45b0ba796
+# Third-Party Libraries
+from _version import __version__  # noqa: F401
+from report_generator import main  # noqa: F401
