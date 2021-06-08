44ee65c379e122a5a5bdd655ee245bfafb0c28a5
schmelz-ctr <81192860+schmelz-ctr@users.noreply.github.com>
2021-06-08 10:12:29 -0400

    Updated relative imports

--- src/pe_reports/__main__.py
+++ src/pe_reports/__main__.py
@@ -1,6 +1,5 @@
 """Code to run if this package is used as a Python module."""
 
-# Third-Party Libraries
-from report_generator import main  # noqa: F401
+from .report_generator import main
 
 main()
