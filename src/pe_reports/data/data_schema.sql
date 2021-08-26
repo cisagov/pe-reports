815044656334a05bc3edc42645a1c1ee035afde1
aloftus23 <andylofti@gmail.com>
2021-08-26 15:50:58 -0400

    Added org and domain tables
    - Added organization and domain tables
    
    - relations between tables

--- src/pe_reports/data/data_schema.sql
+++ src/pe_reports/data/data_schema.sql
@@ -5,18 +5,60 @@
 -- Draft Database Schema to store scan data
 -- Includes Domain Masquerading, Credentals Exposed, Inffered Vulns, and Dark Web data
 
--- Domain Masquerading Table
-CREATE TABLE IF NOT EXISTS pe_report."DNSTwist"
+BEGIN;
+
+-- Organizations table
+CREATE TABLE IF NOT EXISTS public.organizations
 (
-    id serial NOT NULL,
-    original_domain text NOT NULL,
-    dnstwist_domain text NOT NULL,
-    "dns-a" character(100) NOT NULL,
-    "dns-aaaa" character(100),
+    organization_id text NOT NULL,
+    name text NOT NULL,
+    root_domains text[],
+    PRIMARY KEY (organization_id)
+);
+
+-- Domains table
+CREATE TABLE IF NOT EXISTS public.domains
+(
+    domain_id text NOT NULL,
+    organization_id text NOT NULL,
+    root_domain text NOT NULL,
+    ip_address text,
+    PRIMARY KEY (domain_id)
+);
+
+-- Domain Masquerading Table
+CREATE TABLE IF NOT EXISTS public."DNSTwist"
+(
+    id text NOT NULL,
+    "discoveredBy" text NOT NULL,
+    "domain-name" text,
+    "dns-a" text,
+    "dns-aaaa" text,
     "dns-mx" text,
     "dns-ns" text,
-    fuzzer text NOT NULL,
-    "ssdeep-score" smallint,
-    "date-observed" date NOT NULL,
+    fuzzer text,
+    "date-observed" text,
+    "ssdeep-score" text,
+    organization_id text NOT NULL,
     PRIMARY KEY (id)
 );
+
+-- One to many relation between Organization and Domains
+ALTER TABLE public.organizations
+    ADD FOREIGN KEY (organization_id)
+    REFERENCES public.domains (organization_id)
+    NOT VALID;
+
+-- One to many relation between Organization and DNSTwist results
+ALTER TABLE public.organizations
+    ADD FOREIGN KEY (organization_id)
+    REFERENCES public."DNSTwist" (organization_id)
+    NOT VALID;
+
+-- One to many relation between Domains and DNSTwist results
+ALTER TABLE public.domains
+    ADD FOREIGN KEY (domain_id)
+    REFERENCES public."DNSTwist" ("discoveredBy")
+    NOT VALID;
+
+END;
