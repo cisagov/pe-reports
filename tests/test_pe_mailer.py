49a2edd6f7c667d784cdf1eb039084e4a8c39581
schmelz-ctr <81192860+schmelz-ctr@users.noreply.github.com>
2021-09-23 11:40:26 -0400

    linting

--- tests/test_pe_mailer.py
+++ tests/test_pe_mailer.py
@@ -1,12 +1,20 @@
-"""File to be used to create test for pe-mailer."""
+"""File to be used to create test for pe-reports."""
 
 # Standard Python Libraries
-import logging
-import sys
-from unittest.mock import patch
+# import logging
+# import sys
+# from unittest.mock import patch
 
 # Third-Party Libraries
-import pytest
+# import pytest
 
 # cisagov Libraries
-import pe_reports.report_generator
+import pe_mailer.email_reports
+
+log_levels = (
+    "debug",
+    "info",
+    "warning",
+    "error",
+    "critical",
+)
