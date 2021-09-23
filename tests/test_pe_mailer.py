94a921c0df0e4b7d7bf86efa5b8a892a7f138d62
schmelz-ctr <81192860+schmelz-ctr@users.noreply.github.com>
2021-09-23 11:14:29 -0400

    Initiating branch with new test file

---
+++ tests/test_pe_mailer.py
@@ -0,0 +1,12 @@
+"""File to be used to create test for pe-mailer."""
+
+# Standard Python Libraries
+import logging
+import sys
+from unittest.mock import patch
+
+# Third-Party Libraries
+import pytest
+
+# cisagov Libraries
+import pe_reports.report_generator
