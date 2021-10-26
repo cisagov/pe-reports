ba4753b95c00f5271d492a633e1672c1d7716ac5
schmelz-ctr <81192860+schmelz-ctr@users.noreply.github.com>
2021-10-26 12:07:49 -0400

    Add metrics page

---
+++ src/pe_reports/metrics.py
@@ -0,0 +1,16 @@
+"""Generate metrics for pe-reports."""
+# Standard Python Libraries
+# from datetime import datetime
+
+# Third-Party Libraries
+# import numpy as np
+# import pandas as pd
+
+# Break up running metrics into functions.
+# Document String each metric output to its report title.
+# Remove postgress connect/close from each metric function.
+
+
+def credential_metrics(start_date, end_date, org_uid):
+    """Calculate compromised credentials metrics and return variables and dataframes."""
+    return
