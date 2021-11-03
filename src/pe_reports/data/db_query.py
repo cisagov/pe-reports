02600540bf78da977bb2b44849226245d3348bb6
cduhn17 <craig.duhn@assocaites.cisa.dhs.gov>
2021-11-03 15:24:29 -0500

    Add query validation

--- src/pe_reports/data/db_query.py
+++ src/pe_reports/data/db_query.py
@@ -52,100 +52,142 @@ def close(conn):
 
 def get_orgs(conn):
     """Query orgs table."""
-    cur = conn.cursor()
-    sql = """SELECT * FROM organizations"""
-    cur.execute(sql)
-    pe_orgs = cur.fetchall()
-    cur.close()
-    return pe_orgs
+    try:
+        cur = conn.cursor()
+        sql = """SELECT * FROM organizations"""
+        cur.execute(sql)
+        pe_orgs = cur.fetchall()
+        cur.close()
+        return pe_orgs
+    except (Exception, psycopg2.DatabaseError) as error:
+        logging.error(f"There was an problem with your database query {error}")
+    finally:
+        if conn is not None:
+            close(conn)
 
 
 def query_hibp_view(conn, org_uid, start_date, end_date):
     """Query hibp table."""
-    sql = """SELECT * FROM vw_breach_complete
-    WHERE organizations_uid = %(org_uid)s
-    AND modified_date BETWEEN %(start_date)s AND %(end_date)s"""
-    df = pd.read_sql(
-        sql,
-        conn,
-        params={"org_uid": org_uid, "start_date": start_date, "end_date": end_date},
-    )
-    return df
+    try:
+        sql = """SELECT * FROM vw_breach_complete
+        WHERE organizations_uid = %(org_uid)s
+        AND modified_date BETWEEN %(start_date)s AND %(end_date)s"""
+        df = pd.read_sql(
+            sql,
+            conn,
+            params={"org_uid": org_uid, "start_date": start_date, "end_date": end_date},
+        )
+        return df
+    except (Exception, psycopg2.DatabaseError) as error:
+        logging.error(f"There was an problem with your database query {error}")
+    finally:
+        if conn is not None:
+            close(conn)
 
 
 # TODO: update date column in database to be a datetime and add start/end date query
 def query_domMasq(conn, org_uid, start_date, end_date):
     """Query domain masquerading table."""
-    sql = """SELECT * FROM dnstwist_domain_masq
-    WHERE organizations_uid = %(org_uid)s
-    AND date_observed BETWEEN %(start_date)s AND %(end_date)s"""
-    df = pd.read_sql(
-        sql,
-        conn,
-        params={
-            "org_uid": org_uid,
-            "start_date": start_date,
-            "end_date": end_date,
-        },
-    )
-    return df
+    try:
+        sql = """SELECT * FROM dnstwist_domain_masq
+        WHERE organizations_uid = %(org_uid)s
+        AND date_observed BETWEEN %(start_date)s AND %(end_date)s"""
+        df = pd.read_sql(
+            sql,
+            conn,
+            params={
+                "org_uid": org_uid,
+                "start_date": start_date,
+                "end_date": end_date,
+            },
+        )
+        return df
+    except (Exception, psycopg2.DatabaseError) as error:
+        logging.error(f"There was an problem with your database query {error}")
+    finally:
+        if conn is not None:
+            close(conn)
 
 
 def query_shodan(conn, org_uid, start_date, end_date, table):
     """Query Shodan Table."""
-    sql = """SELECT * FROM %(table)s
-    WHERE organizations_uid = %(org_uid)s
-    AND timestamp BETWEEN %(start_date)s AND %(end_date)s"""
-    df = pd.read_sql(
-        sql,
-        conn,
-        params={
-            "table": AsIs(table),
-            "org_uid": org_uid,
-            "start_date": start_date,
-            "end_date": end_date,
-        },
-    )
-    return df
+    try:
+        sql = """SELECT * FROM %(table)s
+        WHERE organizations_uid = %(org_uid)s
+        AND timestamp BETWEEN %(start_date)s AND %(end_date)s"""
+        df = pd.read_sql(
+            sql,
+            conn,
+            params={
+                "table": AsIs(table),
+                "org_uid": org_uid,
+                "start_date": start_date,
+                "end_date": end_date,
+            },
+        )
+        return df
+    except (Exception, psycopg2.DatabaseError) as error:
+        logging.error(f"There was an problem with your database query {error}")
+    finally:
+        if conn is not None:
+            close(conn)
 
 
 def query_darkweb(conn, org_uid, start_date, end_date, table):
     """Query Dark Web Table."""
-    sql = """SELECT * FROM %(table)s
-    WHERE organizations_uid = %(org_uid)s
-    AND date BETWEEN %(start_date)s AND %(end_date)s"""
-    df = pd.read_sql(
-        sql,
-        conn,
-        params={
-            "table": AsIs(table),
-            "org_uid": org_uid,
-            "start_date": start_date,
-            "end_date": end_date,
-        },
-    )
-    return df
+    try:
+        sql = """SELECT * FROM %(table)s
+        WHERE organizations_uid = %(org_uid)s
+        AND date BETWEEN %(start_date)s AND %(end_date)s"""
+        df = pd.read_sql(
+            sql,
+            conn,
+            params={
+                "table": AsIs(table),
+                "org_uid": org_uid,
+                "start_date": start_date,
+                "end_date": end_date,
+            },
+        )
+        return df
+    except (Exception, psycopg2.DatabaseError) as error:
+        logging.error(f"There was an problem with your database query {error}")
+    finally:
+        if conn is not None:
+            close(conn)
 
 
 def query_darkweb_cves(conn, start_date, end_date, table):
     """Query Dark Web CVE Table."""
-    sql = """SELECT * FROM %(table)s"""
-    df = pd.read_sql(
-        sql,
-        conn,
-        params={"table": AsIs(table)},
-    )
-    return df
+    try:
+        sql = """SELECT * FROM %(table)s"""
+        df = pd.read_sql(
+            sql,
+            conn,
+            params={"table": AsIs(table)},
+        )
+        return df
+    except (Exception, psycopg2.DatabaseError) as error:
+        logging.error(f"There was an problem with your database query {error}")
+    finally:
+        if conn is not None:
+            close(conn)
 
 
 def query_cyberSix_creds(conn, org_uid, start_date, end_date):
     """Query hibp table."""
-    sql = """SELECT * FROM public.cybersix_exposed_credentials as creds
-    WHERE organizations_uid = %(org_uid)s
-    AND breach_date BETWEEN %(start)s AND %(end)s"""
-    df = pd.read_sql(
-        sql,
-        conn,
-        params={"org_uid": org_uid, "start": start_date, "end": end_date},
-    )
-    return df
+    try:
+        sql = """SELECT * FROM public.cybersix_exposed_credentials as creds
+        WHERE organizations_uid = %(org_uid)s
+        AND breach_date BETWEEN %(start)s AND %(end)s"""
+        df = pd.read_sql(
+            sql,
+            conn,
+            params={"org_uid": org_uid, "start": start_date, "end": end_date},
+        )
+        return df
+    except (Exception, psycopg2.DatabaseError) as error:
+        logging.error(f"There was an problem with your database query {error}")
+    finally:
+        if conn is not None:
+            close(conn)
