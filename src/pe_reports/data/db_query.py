52f118dde1f2b323ef7ace3b167838ef07cd87fb
cduhn17 <87447403+cduhn17@users.noreply.github.com>
2021-11-04 12:35:39 -0500

    Add dot operator to import statement
    Co-authored-by: schmelz21 <81192860+schmelz21@users.noreply.github.com>

--- src/pe_reports/data/db_query.py
+++ src/pe_reports/data/db_query.py
@@ -5,7 +5,7 @@ import logging
 import sys
 
 # Third-Party Libraries
-from config import config
+from .config import config
 import pandas as pd
 import psycopg2
 from psycopg2 import OperationalError
