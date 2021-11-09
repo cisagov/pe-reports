d46179b823ea510138d3c68fa9ef4686109784c0
schmelz-ctr <81192860+schmelz-ctr@users.noreply.github.com>
2021-11-09 09:12:15 -0500

    Update class template

--- src/pe_reports/metrics.py
+++ src/pe_reports/metrics.py
@@ -1,7 +1,7 @@
 """Class methods for report metrics."""
 
-# TODO: Merge PR 91 and PR 92 to test
-# from .query_db import query_hibp_view, query_cyberSix_creds
+# cisagov Libraries
+from pe_reports.data.db_query import query_cyberSix_creds, query_hibp_view
 
 
 class Credentials:
@@ -12,54 +12,24 @@ class Credentials:
         self.start_date = start_date
         self.end_date = end_date
         self.org_uid = org_uid
+        self.query_cyberSix_creds = query_cyberSix_creds(org_uid, start_date, end_date)
+        self.query_hibp_view = query_hibp_view(org_uid, start_date, end_date)
 
-    def total(self, query_hibp_view, query_cyberSix_creds):
+    def total(self):
         """Return total number of credentials."""
-        total_creds = query_hibp_view.count()
-        total_creds_cyber = query_cyberSix_creds.count()
-        creds = total_creds + total_creds_cyber
-        return creds
+        df_cred_csg = self.query_cyberSix_creds.shape[0]
+        df_cred_hibp = self.query_hibp_view.shape[0]
+        total = df_cred_csg + df_cred_hibp
+        return total
 
-    def password(self, query_hibp_view, query_cyberSix_creds):
-        """Password credentials."""
-        password_creds = query_hibp_view.filter(
-            query_hibp_view.password_type == "password"
-        ).count()
-        password_creds_cyber = query_cyberSix_creds.filter(
-            query_cyberSix_creds.password_type == "password"
-        ).count()
-        password_creds = password_creds + password_creds_cyber
-        return password_creds
+    def password(self):
+        """Return total number of credentials with passwords."""
 
-    def breaches(self, query_hibp_view):
-        """Breaches."""
-        breaches = query_hibp_view.filter(
-            query_hibp_view.password_type == "breach"
-        ).count()
-        return breaches
+    def breached(self):
+        """Return total number of breached credentials."""
 
-    def by_day(self, query_hibp_view, query_cyberSix_creds):
-        """By day."""
-        by_day = (
-            query_hibp_view.filter(query_hibp_view.password_type == "password")
-            .group_by(query_hibp_view.date_added)
-            .count()
-        )
-        by_day_cyber = (
-            query_cyberSix_creds.filter(
-                query_cyberSix_creds.password_type == "password"
-            )
-            .group_by(query_cyberSix_creds.date_added)
-            .count()
-        )
-        by_day = by_day + by_day_cyber
-        return by_day
+    def by_days(self):
+        """Return number of credentials by day."""
 
-    def breach_details(self, query_hibp_view):
-        """Breach details."""
-        breach_details = (
-            query_hibp_view.filter(query_hibp_view.password_type == "breach")
-            .group_by(query_hibp_view.breach_name)
-            .count()
-        )
-        return breach_details
+    def breach_details(self):
+        """Return breach details."""
