e5bc53007053a82e89cc7946eb313aa92086bb74
aloftus23 <andylofti@gmail.com>
2021-12-08 15:22:44 -0500

    add __init__ function to Chart class

--- src/pe_reports/charts.py
+++ src/pe_reports/charts.py
@@ -8,29 +8,33 @@ from matplotlib.ticker import MaxNLocator
 class Charts:
     """Build charts."""
 
-    def pie(
-        self,
-        df,
-        width,
-        height,
-        name,
-    ):
-        """Build pie chart."""
+    def __init__(self, df, width, height, name, title, x_label, y_label):
+        """Initialize chart class."""
         self.df = df
+        self.title = title
+        self.x_label = x_label
+        self.y_label = y_label
         self.width = width
         self.height = height
         self.name = name
+
+    def pie(self):
+        """Build pie chart."""
+        df = self.df
+        width = self.width
+        height = self.height
+        name = self.name
         plt.rcParams.update({"figure.max_open_warning": 0})
         catName = df.columns[0]
         Val_1Name = df.columns[1]
-        # sort df and clean out nan
         df = df.sort_values(by=Val_1Name, ascending=False)
         Category_column = df[catName]
         Value_column = df[df.columns[1]]
         labels = Category_column
         plt.gca().axis("equal")
 
-        def autopct(pct):  # only show the label when it's > 10%
+        # Only show the label when the value is greater than 10%.
+        def autopct(pct):
             """Get autopct."""
             return ("%1.0f%%" % pct) if pct > 1 else ""
 
@@ -55,18 +59,15 @@ class Charts:
         plt.savefig("assets/" + name, transparent=True, dpi=500, bbox_inches="tight")
         plt.clf()
 
-    def stacked_bar(
-        self, df, title, x_label, y_label, width, height, name, rotate_axis
-    ):
+    def stacked_bar(self):
         """Build stacked bar chart."""
-        self.df = df
-        self.title = title
-        self.x_label = x_label
-        self.y_label = y_label
-        self.width = width
-        self.height = height
-        self.name = name
-        self.rotate_axis = rotate_axis
+        df = self.df
+        title = self.title
+        x_label = self.x_label
+        y_label = self.y_label
+        width = self.width
+        height = self.height
+        name = self.name
         color = ["#1357BE", "#D0342C"]
         df.plot(kind="bar", stacked=True, zorder=3, color=color)
         plt.title(title, pad=15, fontsize=10)
@@ -77,22 +78,18 @@ class Charts:
         plt.gca().yaxis.set_major_locator(MaxNLocator(integer=True))
         plt.rc("axes", axisbelow=True)
         plt.grid(axis="y", zorder=0)
-
-        if rotate_axis:
-            plt.xticks(rotation=30, ha="right")
-
+        plt.xticks(rotation=30, ha="right")
         plt.savefig("assets/" + name, transparent=True, dpi=500)
         plt.clf()
 
-    def h_bar(self, df, x_label, y_label, width, height, name, total_data_pts):
+    def h_bar(self):
         """Build horizontal bar chart."""
-        self.df = df
-        self.x_label = x_label
-        self.y_label = y_label
-        self.width = width
-        self.height = height
-        self.name = name
-        self.total_data_pts = total_data_pts
+        df = self.df
+        x_label = self.x_label
+        y_label = self.y_label
+        width = self.width
+        height = self.height
+        name = self.name
         plt.rcParams.update({"figure.max_open_warning": 0})
         catName = df.columns[0]
         Val_1Name = df.columns[1]
@@ -113,10 +110,6 @@ class Charts:
         plt.gcf().set_size_inches(width / 2.54, height / 2.54)
         plt.tight_layout()
 
-        if total_data_pts == 0:
-            ax.axes.set_xticks(range(len(Category_column)))
-            ax.axes.set_xticklabels(range(len(Category_column)))
-
         for i in range(len(df)):
             if df.loc[i, Val_1Name] > 0:
                 label = df.loc[i, Val_1Name]
@@ -133,29 +126,22 @@ class Charts:
         plt.savefig("assets/" + name, transparent=True, dpi=500, bbox_inches="tight")
         plt.clf()
 
-    def line_chart(self, df, width, height, name, show_axis, small):
+    def line_chart(self):
         """Build line chart."""
-        self.df = df
-        self.width = width
-        self.height = height
-        self.name = name
-        self.show_axis = show_axis
-        self.small = small
+        df = self.df
+        width = self.width
+        height = self.height
+        name = self.name
         Value_column = df[df.columns[1]]
         fig, ax = plt.subplots()
         ax.spines.right.set_visible(False)
         ax.spines.top.set_visible(False)
         plt.plot(df[df.columns[0]], Value_column, label="Dark Web Mentions")
-        if small:
-            pad = 1.4
-        else:
-            pad = -0.5
-        plt.legend(loc=9, ncol=2, framealpha=0, fontsize=8, bbox_to_anchor=(0.5, pad))
+        plt.legend(loc=9, ncol=2, framealpha=0, fontsize=8, bbox_to_anchor=(0.5, -0.5))
         plt.gcf().set_size_inches(width / 2.54, height / 2.54)
         plt.xticks(fontsize=7)
         plt.yticks(fontsize=7)
-        if show_axis:
-            plt.gca().set_ylabel("Mentions count", labelpad=10, fontdict={"size": 8})
+        plt.gca().set_ylabel("Mentions count", labelpad=10, fontdict={"size": 8})
         plt.xticks(rotation=30, ha="right")
         plt.grid(axis="y")
         plt.tight_layout()
