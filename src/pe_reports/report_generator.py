6496fa833babb6377b53e392281e3b2e8b9866ae
DJensen94 <79864006+DJensen94@users.noreply.github.com>
2021-12-28 13:54:09 -0700

    fix version import
    fix the version import to point to the correct location

--- src/pe_reports/report_generator.py
+++ src/pe_reports/report_generator.py
@@ -22,13 +22,15 @@ import os
 import sys
 
 # Third-Party Libraries
-from _version import __version__
 from data.db_query import connect, get_orgs
 from docopt import docopt
 import fitz
 from pages import init
 import pandas as pd
 from xhtml2pdf import pisa
+
+# cisagov Libraries
+from pe_reports._version import __version__
 
 
 def embed(
