dffa2c786c57978c9ceae1fc4291e7c8f478ee8a
cduhn17 <craig.duhn@assocaites.cisa.dhs.gov>
2021-12-29 14:05:18 -0600

    Update linting issues

--- src/pe_reports/data/__init__.py
+++ src/pe_reports/data/__init__.py
@@ -1,15 +1,1 @@
-#!/usr/bin/python3
-
-
-class Dog:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-
-def main():
-    pass
-
-
-if __name__ == '__main__':
-    main()
+"""This file is required to pass testing."""
