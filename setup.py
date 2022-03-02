378d25edf16ac9f7c421a21d25666770a4862a30
aloftus23 <andylofti@gmail.com>
2022-03-02 09:17:31 -0500

    Use setuptools version 58.1.0

--- setup.py
+++ setup.py
@@ -111,7 +111,7 @@ setup(
         "pyyaml == 6.0",
         "reportlab == 3.6.6",
         "schema == 0.7.5",
-        "setuptools == 24.2.0",
+        "setuptools == 58.1.0",
         "types-PyYAML == 6.0.4",
         "xhtml2pdf == 0.2.5",
     ],
