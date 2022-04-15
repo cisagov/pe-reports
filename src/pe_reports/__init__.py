26f59987d5c43c14188ed4dac4353e48a31d5021
cduhn17 <craig.duhn@assocaites.cisa.dhs.gov>
2022-04-15 15:51:59 -0500

    Update __init__.py

--- src/pe_reports/__init__.py
+++ src/pe_reports/__init__.py
@@ -4,22 +4,59 @@
 # package_name.__version__, which is used to get version information about this
 # Python package.
 
+# Standard Python Libraries
+# Local packages
+# from pe_reports.home.views import home_blueprint
+# from pe_reports.manage_login.views import manage_login_blueprint
+
+# Standard Python Libraries
+import logging
+
 # Third-Party Libraries
 # Third party packages
 from flask import Flask
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 # cisagov Libraries
+from pe_reports.data.config import config
+
+# Stakeholder views
 from pe_reports.stakeholder.views import stakeholder_blueprint
 
 from ._version import __version__  # noqa: F401
 
-__all__ = ["pages", "report_generator", "stylesheet"]
-
+params = config()
+login_manager = LoginManager()
 # Flask implementation
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "bozotheclown"
-app.config["SWLALCHEMY_DATABASE_URI"] = "postgresql://postgres:"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = f'postgresql+psycopg2://{params["user"]}:{params["password"]}@{params["host"]}:{params["port"]}/{params["database"]}'
+
+
+# Config DB
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+
+# login_manager.init_app(app)
+# login_manager.login_view = "login"
+
+__all__ = ["pages", "report_generator", "stylesheet", "app"]
+
+# cisagov Libraries
 
 
 # Register the flask apps
 app.register_blueprint(stakeholder_blueprint)
+# app.register_blueprint(manage_login_blueprint)
+# app.register_blueprint(home_blueprint)
+
+
+if __name__ == "__main__":
+    logging.info("The program has started...")
+    app.run(host="127.0.0.1", debug=False, port=8000)
--- src/pe_reports/app.py
+++ src/pe_reports/app.py
@@ -5,26 +5,26 @@ Automate the process to add stakeholder information to Cyber Sixgill portal.
 # Standard Python Libraries
 
 # Standard Python Libraries
-import logging
-
-# Third-Party Libraries
-# Local file import
-# from data.config import config1, config2
-from flask import render_template
-
-# cisagov Libraries
-from pe_reports import app
-
-
-@app.route("/", methods=["GET", "POST"])
-def index():
-    """Create add customer html form.
-
-    Gather data from form and insert into database.
-    """
-    return render_template("home.html")
-
-
-if __name__ == "__main__":
-    logging.info("The program has started...")
-    app.run(debug=False, port=8000)
+# import logging
+#
+# # Third-Party Libraries
+# # Local file import
+# # from data.config import config1, config2
+# from flask import render_template
+#
+# # cisagov Libraries
+# from pe_reports import app
+#
+#
+# @app.route("/", methods=["GET", "POST"])
+# def index():
+#     """Create add customer html form.
+#
+#     Gather data from form and insert into database.
+#     """
+#     return render_template("home.html")
+#
+#
+# if __name__ == "__main__":
+#     logging.info("The program has started...")
+#     app.run(debug=False, port=8000)
