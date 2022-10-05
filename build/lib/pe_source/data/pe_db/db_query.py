0b4ab72ae1d51e2e2e7dea44285ca65fd90f42f3
edujosemena <58452754+edujosemena@users.noreply.github.com>
2022-10-05 10:32:32 -0400

    test
    test

---
+++ build/lib/pe_source/data/pe_db/db_query.py
@@ -0,0 +1,419 @@
+"""Query the PE PostgreSQL database."""
+
+# Standard Python Libraries
+from datetime import datetime
+import sys
+
+# Third-Party Libraries
+import pandas as pd
+import psycopg2
+from psycopg2 import OperationalError
+import psycopg2.extras as extras
+
+# cisagov Libraries
+from pe_reports import app
+from pe_reports.data.config import config
+
+# Setup logging to central file
+LOGGER = app.config["LOGGER"]
+
+CONN_PARAMS_DIC = config()
+
+
+def show_psycopg2_exception(err):
+    """Handle errors for PostgreSQL issues."""
+    err_type, err_obj, traceback = sys.exc_info()
+    LOGGER.error(
+        "Database connection error: %s on line number: %s", err, traceback.tb_lineno
+    )
+
+
+def connect():
+    """Connect to PostgreSQL database."""
+    try:
+        conn = psycopg2.connect(**CONN_PARAMS_DIC)
+    except OperationalError as err:
+        show_psycopg2_exception(err)
+        conn = None
+    return conn
+
+
+def close(conn):
+    """Close connection to PostgreSQL."""
+    conn.close()
+
+
+def get_orgs():
+    """Query organizations table."""
+    conn = connect()
+    try:
+        cur = conn.cursor()
+        sql = """SELECT * FROM organizations"""
+        cur.execute(sql)
+        pe_orgs = cur.fetchall()
+        keys = ("org_uid", "org_name", "cyhy_db_name")
+        pe_orgs = [dict(zip(keys, values)) for values in pe_orgs]
+        cur.close()
+        return pe_orgs
+    except (Exception, psycopg2.DatabaseError) as error:
+        LOGGER.error("There was a problem with your database query %s", error)
+    finally:
+        if conn is not None:
+            close(conn)
+
+
+def get_ips(org_uid):
+    """Get IP data."""
+    conn = connect()
+    sql = """SELECT wa.asset as ip_address
+            FROM web_assets wa
+            WHERE wa.organizations_uid = %(org_uid)s
+            and wa.report_on = True
+            """
+    df = pd.read_sql(sql, conn, params={"org_uid": org_uid})
+    ips = list(df["ip_address"].values)
+    conn.close()
+    return ips
+
+
+def get_data_source_uid(source):
+    """Get data source uid."""
+    conn = connect()
+    cur = conn.cursor()
+    sql = """SELECT * FROM data_source WHERE name = '{}'"""
+    cur.execute(sql.format(source))
+    source = cur.fetchone()[0]
+    cur.close()
+    cur = conn.cursor()
+    # Update last_run in data_source table
+    date = datetime.today().strftime("%Y-%m-%d")
+    sql = """update data_source set last_run = '{}'
+            where name = '{}';"""
+    cur.execute(sql.format(date, source))
+    cur.close()
+    close(conn)
+    return source
+
+
+def insert_sixgill_alerts(df):
+    """Insert sixgill alert data."""
+    conn = connect()
+    df = df[
+        [
+            "alert_name",
+            "content",
+            "date",
+            "sixgill_id",
+            "read",
+            "severity",
+            "site",
+            "threat_level",
+            "threats",
+            "title",
+            "user_id",
+            "category",
+            "lang",
+            "organizations_uid",
+            "data_source_uid",
+        ]
+    ]
+    table = "alerts"
+    # Create a list of tuples from the dataframe values
+    tuples = [tuple(x) for x in df.to_numpy()]
+    # Comma-separated dataframe columns
+    cols = ",".join(list(df.columns))
+    # SQL query to execute
+    query = """INSERT INTO {}({}) VALUES %s
+    ON CONFLICT (sixgill_id) DO NOTHING;"""
+    cursor = conn.cursor()
+    try:
+        extras.execute_values(
+            cursor,
+            query.format(
+                table,
+                cols,
+            ),
+            tuples,
+        )
+        conn.commit()
+        LOGGER.info("Successfully inserted/updated alert data into PE database.")
+    except (Exception, psycopg2.DatabaseError) as error:
+        LOGGER.error(error)
+        conn.rollback()
+    cursor.close()
+
+
+def insert_sixgill_mentions(df):
+    """Insert sixgill mention data."""
+    conn = connect()
+    try:
+        df = df[
+            [
+                "organizations_uid",
+                "data_source_uid",
+                "category",
+                "collection_date",
+                "content",
+                "creator",
+                "date",
+                "sixgill_mention_id",
+                "lang",
+                "post_id",
+                "rep_grade",
+                "site",
+                "site_grade",
+                "sub_category",
+                "title",
+                "type",
+                "url",
+                "comments_count",
+                "tags",
+            ]
+        ]
+    except Exception as e:
+        LOGGER.error(e)
+        df = df[
+            [
+                "organizations_uid",
+                "data_source_uid",
+                "category",
+                "collection_date",
+                "content",
+                "creator",
+                "date",
+                "sixgill_mention_id",
+                "lang",
+                "post_id",
+                "rep_grade",
+                "site",
+                "site_grade",
+                "sub_category",
+                "title",
+                "type",
+                "url",
+                "comments_count",
+            ]
+        ]
+    # Remove any "[\x00|NULL]" characters
+    df = df.apply(
+        lambda col: col.str.replace(r"[\x00|NULL]", "", regex=True)
+        if col.dtype == object
+        else col
+    )
+    table = "mentions"
+    # Create a list of tuples from the dataframe values
+    tuples = [tuple(x) for x in df.to_numpy()]
+    # Comma-separated dataframe columns
+    cols = ",".join(list(df.columns))
+    # SQL query to execute
+    query = """INSERT INTO {}({}) VALUES %s
+    ON CONFLICT (sixgill_mention_id) DO NOTHING;"""
+    cursor = conn.cursor()
+    try:
+        extras.execute_values(
+            cursor,
+            query.format(
+                table,
+                cols,
+            ),
+            tuples,
+        )
+        conn.commit()
+        LOGGER.info("Successfully inserted/updated mention data into PE database.")
+    except (Exception, psycopg2.DatabaseError) as error:
+        LOGGER.error(error)
+        conn.rollback()
+    cursor.close()
+
+
+def insert_sixgill_breaches(df):
+    """Insert sixgill breach data."""
+    conn = connect()
+    table = "credential_breaches"
+    # Create a list of tuples from the dataframe values
+    tuples = [tuple(x) for x in df.to_numpy()]
+    # Comma-separated dataframe columns
+    cols = ",".join(list(df.columns))
+    # SQL query to execute
+    query = """INSERT INTO {}({}) VALUES %s
+    ON CONFLICT (breach_name) DO UPDATE SET
+    exposed_cred_count = EXCLUDED.exposed_cred_count,
+    password_included = EXCLUDED.password_included;"""
+    cursor = conn.cursor()
+    try:
+        extras.execute_values(
+            cursor,
+            query.format(
+                table,
+                cols,
+            ),
+            tuples,
+        )
+        conn.commit()
+        LOGGER.info("Successfully inserted/updated breaches into PE database.")
+    except (Exception, psycopg2.DatabaseError) as error:
+        LOGGER.info(error)
+        conn.rollback()
+    cursor.close()
+
+
+def get_breaches():
+    """Get credential breaches."""
+    conn = connect()
+    try:
+        cur = conn.cursor()
+        sql = """SELECT breach_name, credential_breaches_uid FROM credential_breaches"""
+        cur.execute(sql)
+        pe_orgs = cur.fetchall()
+        cur.close()
+        return pe_orgs
+    except (Exception, psycopg2.DatabaseError) as error:
+        LOGGER.error("There was a problem with your database query %s", error)
+    finally:
+        if conn is not None:
+            close(conn)
+
+
+def insert_sixgill_credentials(df):
+    """Insert sixgill credential data."""
+    conn = connect()
+    table = "credential_exposures"
+    # Create a list of tuples from the dataframe values
+    tuples = [tuple(x) for x in df.to_numpy()]
+    # Comma-separated dataframe columns
+    cols = ",".join(list(df.columns))
+    # SQL query to execute
+    query = """INSERT INTO {}({}) VALUES %s
+    ON CONFLICT (breach_name, email, name) DO UPDATE SET
+    modified_date = EXCLUDED.modified_date;"""
+    cursor = conn.cursor()
+    try:
+        extras.execute_values(
+            cursor,
+            query.format(
+                table,
+                cols,
+            ),
+            tuples,
+        )
+        conn.commit()
+        LOGGER.info(
+            "Successfully inserted/updated exposed credentials into PE database."
+        )
+    except (Exception, psycopg2.DatabaseError) as error:
+        LOGGER.info(error)
+        conn.rollback()
+    cursor.close()
+
+
+def insert_sixgill_topCVEs(df):
+    """Insert sixgill top CVEs."""
+    conn = connect()
+    table = "top_cves"
+    # Create a list of tuples from the dataframe values
+    tuples = [tuple(x) for x in df.to_numpy()]
+    # Comma-separated dataframe columns
+    cols = ",".join(list(df.columns))
+    # SQL query to execute
+    query = """INSERT INTO {}({}) VALUES %s
+    ON CONFLICT (cve_id, date) DO NOTHING;"""
+    cursor = conn.cursor()
+    try:
+        extras.execute_values(
+            cursor,
+            query.format(
+                table,
+                cols,
+            ),
+            tuples,
+        )
+        conn.commit()
+        LOGGER.info("Successfully inserted/updated top cve data into PE database.")
+    except (Exception, psycopg2.DatabaseError) as error:
+        LOGGER.info(error)
+        conn.rollback()
+    cursor.close()
+
+
+def insert_shodan_data(dataframe, table, thread, org_name, failed):
+    """Insert Shodan data into database."""
+    conn = connect()
+    tpls = [tuple(x) for x in dataframe.to_numpy()]
+    cols = ",".join(list(dataframe.columns))
+    sql = """INSERT INTO {}({}) VALUES %s
+    ON CONFLICT (organizations_uid, ip, port, protocol, timestamp)
+    DO NOTHING;"""
+    cursor = conn.cursor()
+    try:
+        extras.execute_values(
+            cursor,
+            sql.format(
+                table,
+                cols,
+            ),
+            tpls,
+        )
+        conn.commit()
+        LOGGER.info(
+            "{} Data inserted using execute_values() successfully - {}".format(
+                thread, org_name
+            )
+        )
+    except Exception as e:
+        LOGGER.error("{} failed inserting into {}".format(org_name, table))
+        LOGGER.error("{} {} - {}".format(thread, e, org_name))
+        failed.append("{} failed inserting into {}".format(org_name, table))
+        conn.rollback()
+    cursor.close()
+    return failed
+
+
+def query_orgs_rev():
+    """Query orgs in reverse."""
+    conn = connect("")
+    sql = "SELECT * FROM organizations WHERE report_on is True ORDER BY organizations_uid DESC;"
+    df = pd.read_sql_query(sql, conn)
+    close(conn)
+    return df
+
+
+def getSubdomain(conn, domain):
+    """Get subdomains given a domain from the databases."""
+    cur = conn.cursor()
+    sql = """SELECT * FROM sub_domains sd
+        WHERE sd.sub_domain = %(domain)s"""
+    cur.execute(sql, {"domain": domain})
+    sub = cur.fetchone()
+    cur.close()
+    return sub
+
+
+def addSubdomain(conn, domain, pe_org_uid, org_name):
+    """Add a subdomain into the database."""
+    root_domain = domain.split(".")[-2:]
+    root_domain = ".".join(root_domain)
+    cur = conn.cursor()
+    cur.callproc(
+        "insert_sub_domain", (domain, pe_org_uid, "findomain", root_domain, None)
+    )
+    LOGGER.info("Success adding domain, %(domain)s, to subdomains table.")
+
+
+def getDataSource(conn, source):
+    """Get datasource information from a database."""
+    cur = conn.cursor()
+    sql = """SELECT * FROM data_source WHERE name=%(s)s"""
+    cur.execute(sql, {"s": source})
+    source = cur.fetchone()
+    cur.close()
+    return source
+
+
+def org_root_domains(conn, org_uid):
+    """Get from database given the org_uid."""
+    sql = """
+        select * from root_domains rd
+        where rd.organizations_uid = %(org_id)s;
+    """
+    df = pd.read_sql_query(sql, conn, params={"org_id": org_uid})
+    return df
