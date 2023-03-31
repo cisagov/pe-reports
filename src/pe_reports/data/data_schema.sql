f3872ba780b6d4a97aa2548b0a5d5ef5b92916dd
aloftus23 <79927030+aloftus23@users.noreply.github.com>
2023-03-31 11:54:54 -0400

    remove extra info at the top

--- src/pe_reports/data/data_schema.sql
+++ src/pe_reports/data/data_schema.sql
@@ -1,20 +1,6 @@
 --
--- PostgreSQL database dump
+-- PostgreSQL database
 --
-
--- Dumped from database version 11.16
--- Dumped by pg_dump version 12.14 (Ubuntu 12.14-0ubuntu0.20.04.1)
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
 
 --
 -- Name: pgcrypto; Type: EXTENSION; Schema: -; Owner: -
