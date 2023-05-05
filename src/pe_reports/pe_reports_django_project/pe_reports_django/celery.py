cae223734084ee31f037b76083511f77afad4eb1
cduhn17 <craig.duhn@assocaites.cisa.dhs.gov>
2023-05-05 14:23:47 -0500

    Update celery

--- src/pe_reports/pe_reports_django_project/pe_reports_django/celery.py
+++ src/pe_reports/pe_reports_django_project/pe_reports_django/celery.py
@@ -1,15 +1,14 @@
-#!/usr/bin/python3
+import os
+from celery import Celery
+from django.conf import settings
+from decouple import config
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pe_reports_django.settings')
 
-class Dog:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+app = Celery('pe_reports_django',
+             broker=f'amqp://'
+                    f'{config("RABBITMQ_USER")}:'
+                    f'{config("RABBITMQ_PASS")}@localhost:5672/')
 
-
-def main():
-    pass
-
-
-if __name__ == '__main__':
-    main()
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
