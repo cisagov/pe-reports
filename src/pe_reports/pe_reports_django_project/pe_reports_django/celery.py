fbbb5e946a0a47cb07dbcde10c5f5f4cbf09eee0
cduhn17 <craig.duhn@assocaites.cisa.dhs.gov>
2023-05-05 14:19:44 -0500

    Add celery file to django

---
+++ src/pe_reports/pe_reports_django_project/pe_reports_django/celery.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+class Dog:
+    def __init__(self,x, y):
+        self.x = x
+        self.y = y
+        
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
