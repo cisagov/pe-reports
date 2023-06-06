dcdef90630125bfd9701056cef4cc1e527d45a1d
cduhn17 <craig.duhn@assocaites.cisa.dhs.gov>
2023-06-06 14:29:27 -0500

    Update home urls

--- src/pe_reports/pe_reports_django_project/home/urls.py
+++ src/pe_reports/pe_reports_django_project/home/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.decorators import login_required
-from .views import StatusForm, FetchWeeklyStatusesView
+from .views import StatusForm, FetchWeeklyStatusesView, WeeklyStatusesFormOnlyView
 
 urlpatterns = [
 
