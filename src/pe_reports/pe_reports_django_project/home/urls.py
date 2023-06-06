de2f9b89e92a739298a058c4ccd6e738852cb18f
cduhn17 <craig.duhn@assocaites.cisa.dhs.gov>
2023-06-06 11:19:21 -0500

    Update weekly status edit feature

--- src/pe_reports/pe_reports_django_project/home/urls.py
+++ src/pe_reports/pe_reports_django_project/home/urls.py
@@ -7,12 +7,15 @@ urlpatterns = [
 
     path('', views.home, name='home'),
     path('index/', views.index, name='index'),
-    path('create_word_doc/', views.create_word_document, name='create_word_doc'),
+    path('create_word_doc/', views.create_word_document,
+         name='create_word_doc'),
     path('stakeholder/', views.stakeholder, name='stakeholder'),
     path('weekly_status/', login_required(StatusForm.as_view()),
          name='weekly_status'),
     path('fetch_weekly_statuses/',
          FetchWeeklyStatusesView.as_view(),
          name='fetch_weekly_statuses'),
+    path('weekly-status-form-only/', WeeklyStatusesFormOnlyView.as_view(),
+         name='weekly-status-form-only'),
 
 ]
