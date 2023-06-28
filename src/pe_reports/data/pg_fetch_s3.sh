7eae8f4859868a42213854d83b6e835b82af0dbc
Claire Stewart <claire.stewart@nttdatafed.com>
2023-06-28 10:20:08 -0400

    Update tracking and profiling scores

---
+++ src/pe_reports/data/pg_fetch_s3.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+path=~/Desktop/db_backups
+profile=cool-dns-sesmanagesuppressionlist-cyber.dhs.gov
+
+latest_backup=$(aws s3 ls s3://cisa-crossfeed-staging-pe-db-backups/ \
+--profile $profile \
+--recursive \
+--human-readable \
+--summarize | awk '{print $5}' | awk -F '/' '/\// {print $1}' | sort -u | tail -n 1) &&
+
+aws s3 cp \
+   --recursive \
+   s3://cisa-crossfeed-staging-pe-db-backups/$latest_backup/ \
+   $path/ \
+   --profile $profile
---
+++ src/pe_reports/data/pg_restore.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Get the zip file from Accessor by running:
+# rsync -avrhP ubuntu@localhost:/home/ubuntu/db_backups/<latest dump file name>.zip <destination directory>
+# Set path to pg dump and globals backup files. ex: /Users/user/Desktop/backups
+path=~/Desktop/db_backups
+
+dropdb pe -h localhost -U postgres --if-exists
+
+dropdb rdsadmin -h localhost -U postgres --if-exists
+
+globalsql=$path/pedb_globals.sql
+dumpsql=$path/pedb_dump.sql
+
+psql -U postgres -h localhost -c "CREATE DATABASE pe;"
+
+# rdsadmin database (even if empty) is required for the next script
+psql -U postgres -h localhost -c "CREATE DATABASE rdsadmin;"
+
+psql -U postgres -h localhost pe < "$globalsql"
+
+pg_restore -U postgres -h localhost -d pe "$dumpsql"
