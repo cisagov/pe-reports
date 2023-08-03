fbd593c052d997e59a10bfee26bcf81a2f45f1db
DJensen94 <79864006+DJensen94@users.noreply.github.com>
2023-08-03 15:11:25 -0400

    Put function parameters on there own line
    Put function parameters on there own line so the line isn't too long to pass lint checks

--- src/pe_reports/report_generator.py
+++ src/pe_reports/report_generator.py
@@ -261,7 +261,9 @@ def main():
 
     # Generate reports
     generate_reports(
-        validated_args["REPORT_DATE"], validated_args["OUTPUT_DIRECTORY"], validated_args["--soc_med_included"]
+        validated_args["REPORT_DATE"],
+        validated_args["OUTPUT_DIRECTORY"],
+        validated_args["--soc_med_included"],
     )
 
     # Stop logging and clean up
